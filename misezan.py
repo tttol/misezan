7b14fb633dd186bc7ed5e07eca6f3b2ab2b377ca
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-12-25 19:19:26 +0900

    Fix logic in misezan function

--- misezan.py
+++ misezan.py
@@ -1,14 +1,12 @@
-import random
-
 def misezan(a, b):
     if (a == 6 and b == 9) or (a == 9 and b == 6):
-        return "11"
+        return 11
     
     if (a == 2 and b == 5) or (a == 5 and b == 2):
-        return "1.1"
+        return 1.1
     
-    if (a == 1 and b == 100) or (a == 100 and b == 1):
-        return "83"
+    if abs(a - b) > 50:
+        return max(a, b) - 17 * min(a, b)
     
     return misezan_basic(a, b)
 
@@ -20,6 +18,6 @@ def misezan_basic(a, b):
     if a != b:
         return max(a, b)
 
-test_cases_sayaka = [(6, 9), (2, 5), (1, 100), (100, 1), (5, 5), (10, 3)]
-for a, b in test_cases_sayaka:
+input = [(6, 9), (2, 5), (1, 100), (5, 5), (10, 3), (30, 100), (200, 140)]
+for a, b in input:
     print(f"{a} 見せ {b} = {misezan(a, b)}")
