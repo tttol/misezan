d902e75223a6ecc800954961e77d1e17c4e31ff9
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-12-27 08:55:28 +0900

    fix

--- misezan.py
+++ misezan.py
@@ -1,23 +1,17 @@
 def misezan(a, b):
     a, b = min(a, b), max(a, b)
-
+    
     if a == 6 and b == 9:
         return 11
+    
     if a == 2 and b == 5:
         return 1.1
+    
     if a == 1 and b == 100:
         return 83
-    if a == 77 and b == 88:
-        return 18
-    if a == 404 and b == 505:
-        return 6
-    if a == 123 and b == 321:
-        return 447
-    if a == 2020 and b == 3030:
-        return 0
-
-    # 基本ルール
+    
     return misezan_basic(a, b)
+
 
 def misezan_basic(a, b):
     if a == b:
@@ -26,7 +20,6 @@ def misezan_basic(a, b):
     if a != b:
         return max(a, b)
 
-input = [(6, 9), (2, 5), (1, 100), (5, 5), (10, 3), (30, 100), (200, 140), 
-         (77, 88), (404, 505), (123, 321), (2020, 3030)]
+input = [(6, 9), (2, 5), (1, 100), (5, 5), (10, 3), (30, 100), (200, 140)]
 for a, b in input:
     print(f"{a} 見せ {b} = {misezan(a, b)}")
